59ff8e266aeff2787981481d967afecef0938799
MarcieD1 <marciatarisai@icloud.com>
2024-01-31 14:07:31 -0600

    Fixed a Compiling Issue with Binary_Tree_is_perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,45 +1,25 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect_recursive - Helper function to check if a tree is perfect
- * @tree: Pointer to the root node of the tree
- * Return: 0 if not perfect, the level of height otherwise
- */
-int binary_tree_is_perfect_recursive(const binary_tree_t *tree)
-{
-	int l = 0, r = 0;
-
-	if (tree->left && tree->right)
-	{
-	l = 1 + binary_tree_is_perfect_recursive(tree->left);
-	r = 1 + binary_tree_is_perfect_recursive(tree->right);
-
-	if (r == l && r != 0 && l != 0)
-	return (r);
-
-	return (0);
-	}
-	else if (!tree->left && !tree->right)
-	{
-	return (1);
-	}
-	else
-	{
-	return (0);
-	}
-}
-
-/**
- * binary_tree_is_perfect - Check if a binary tree is perfect
- * @tree: Pointer to the root node of the tree
- * Return: 1 if perfect, 0 otherwise
+ * binary_tree_is_perfect - Checks if a binary tree is perfect.
+ *
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise.
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 	return (0);
 
-	return binary_tree_is_perfect_recursive(tree);
+	if (!tree->left && !tree->right)
+	return (1);
+
+	if (tree->left && tree->right)
+	{
+	if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+	return (1);
+	}
+
+	return (0);
 }
