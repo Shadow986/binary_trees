51e121e181b31f98ab5f8c92ac6cc47ad1e11ca3
MarcieD1 <marciatarisai@icloud.com>
2024-01-31 10:45:11 -0600

    Added Prototypes that Include #ifdef, also added the binary tree print to draw the compiler

--- binary_trees.h
+++ binary_trees.h
@@ -1,4 +1,5 @@
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define _BINARY_TREES_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -33,6 +34,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 int binary_tree_is_leaf(const binary_tree_t *node);
+void binary_tree_print(const binary_tree_t *);
 void binary_tree_delete(binary_tree_t *tree);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_root(const binary_tree_t *node);
@@ -46,5 +48,4 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
 #endif
