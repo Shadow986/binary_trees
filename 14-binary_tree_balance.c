19575b6386f0322570facfb7127a83bcbc1856b1
MarcieD1 <marciatarisai@icloud.com>
2024-01-31 14:41:58 -0600

    Fix function that measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,45 +1,46 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - Checks if a binary tree is perfect.
- *
- * @tree: A pointer to the root node of the tree to check.
- *
- * Return: 1 if the tree is perfect, 0 otherwise.
+ * binary_tree_height_b - Measures height of a binary tree for a bal tree
+ * @tree: tree to go through
+ * Return: the height
  */
-int binary_tree_is_perfect(const binary_tree_t *tree)
+
+size_t binary_tree_height_b(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+	size_t l = 0;
+	size_t r = 0;
 
-    if (!tree->left && !tree->right)
-        return (1);
-
-    if (tree->left && tree->right)
-    {
-        if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
-            return (1);
-    }
-
-    return (0);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+			r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+		}
+		return ((l > r) ? l : r);
+	}
 }
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree.
- *
- * @tree: A pointer to the root node of the tree to measure the balance factor.
- *
- * Return: The balance factor of the tree.
+ * binary_tree_balance - Measures balance factor of a binary tree
+ * @tree: tree to go through
+ * Return: balanced factor
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
+	int right = 0, left = 0, total = 0;
 
-    int left_height = binary_tree_height(tree->left);
-    int right_height = binary_tree_height(tree->right);
-
-    int balance_factor = left_height - right_height;
-
-    return (balance_factor);
+	if (tree)
+	{
+		left = ((int)binary_tree_height_b(tree->left));
+		right = ((int)binary_tree_height_b(tree->right));
+		total = left - right;
+	}
+	return (total);
 }
