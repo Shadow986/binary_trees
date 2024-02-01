8a2c45f6ab5c61e0c711ee644518141fc143718c
MarcieD1 <marciatarisai@icloud.com>
2024-02-01 08:30:00 -0600

    Added Prototypes

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -9,10 +9,10 @@
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+	return (0);
 
-    return (binary_tree_is_avl_helper(tree) != -1);
+	return (binary_tree_is_avl_helper(tree) != -1);
 }
 
 /**
@@ -24,21 +24,21 @@ int binary_tree_is_avl(const binary_tree_t *tree)
  */
 int binary_tree_is_avl_helper(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	int left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+	return (0);
 
-    left_height = binary_tree_is_avl_helper(tree->left);
-    if (left_height == -1)
-        return (-1);
+	left_height = binary_tree_is_avl_helper(tree->left);
+	if (left_height == -1)
+	return (-1);
 
-    right_height = binary_tree_is_avl_helper(tree->right);
-    if (right_height == -1)
-        return (-1);
+	right_height = binary_tree_is_avl_helper(tree->right);
+	if (right_height == -1)
+	return (-1);
 
-    if (abs(left_height - right_height) > 1)
-        return (-1);
+	if (abs(left_height - right_height) > 1)
+	return (-1);
 
-    return (1 + MAX(left_height, right_height));
+	return (1 + MAX(left_height, right_height));
 }
